f8d04db092868f304f3b46d495cd26a35247a8de
Amaraa <amarbaatarjanchivsembe@gmail.com>
2023-07-27 15:30:18 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -9,16 +9,28 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shoomii ali talaaraa buusniig hadgalah huvisagch heregtei, 1-6 gesen utgiig ene huvisagchid sanamsarguigeer uusgej ogno.
-var dice = Math.floor(Math.random() * 6 ) + 1;
+
 
 // <div class="player-score" id="score-0">43</div>
 
 
 //document.querySelector('#score-1').innerHTML = '<em>Yes!<em>';
+document.getElementById('score-1').textContent = "0";
+document.getElementById('score-0').textContent = "0";
 
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').style.display = "none";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-console.log("Shoo :" + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png" ;
+});
+
+
+
+
+// console.log("Shoo :" + diceNumber);
