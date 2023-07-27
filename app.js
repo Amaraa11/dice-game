44c0403c5fe91636bf1ba8a7c0173a026d279dba
Amaraa <amarbaatarjanchivsembe@gmail.com>
2023-07-27 19:09:41 +0800

    yalagchiig todruulna

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ diceDom.style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function () {
     //1-6 dotorh sanamsargui 1 too buulgah
     var diceNumber = Math.floor(Math.random() * 6) + 1;
-//shoonii zurgiig web deer gargaj irne
+    //shoonii zurgiig web deer gargaj irne
     diceDom.style.display = "block";
     //buusan shoonii toond hargalzan shoonii zurgiig web deer gargaj irne
     diceDom.src = "dice-" + diceNumber + ".png";
@@ -39,13 +39,39 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 
     } else {
         // 1 buusan tul toglogchiin eeljiig ene hesegt solij ogno.
-         //Ene toglogchiin eeljindee tsugluulsan onoog 0 bolgono
-        roundScore = 0;
+        switchToNextPlayer();
+    
+    }
+});
+
+//HOLD tovchnii event listenting
+document.querySelector(".btn-hold").addEventListener("click", function () {
+    // Ug toglogchiin tsugluulsan eeljnii onoog global onoon deer nemj ogno.
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+
+    // Delgets deer onoog oorchilno.
+    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+  
+    // Ug toglogchiin hojson esehiis (onoo ni 100-s ih eseh) shalgah.
+    if (scores[activePlayer] >= 20) {
+        // Ylagch gesen textiig nerniih ni orond gargana.
+        document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    } else {
+        // Toglogchiin eeljiig solino.
+        switchToNextPlayer();
+    }
+});
+
+// Ene function ni togloh eeljiig daraachiin toglogchruu shiljuuldeg
+function switchToNextPlayer() {
+     // Eeljiin onoog ni 0 bolgono.
+    // Toglogchiin eeljiig solino.
+    roundScore = 0;
         document.getElementById("current-" + activePlayer).textContent = 0;
+
         // toglogchiin eeljiig nogoo talruu shiljuulne
-        //Herev idevhtei toglogch ni 0 baival idevhtei toglogchiig 1 bolgo.
-        //Ugui bol idevhtei toglogchiig 0 bolgo.
-     
         activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
 
         //Ulaan tsegiig shiljuuleh
@@ -54,16 +80,4 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 
         //shoog tur alga bolgoh
         diceDom.style.display = "none";
-        // if (activePlayer === 0) {
-        //     activePlayer = 1;
-
-        // } else {
-        //     activePlayer = 0;
-        // }
-    }
-});
-
-
-
-
-// console.log("Shoo :" + diceNumber);
+};
