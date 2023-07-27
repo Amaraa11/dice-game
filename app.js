a2ce6bcb5fefac139a9d162a3caf7b236264db0f
Amaraa <amarbaatarjanchivsembe@gmail.com>
2023-07-27 16:40:02 +0800

    Toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Togloglogchiin eeljiig hadgalah huvisagch, 1-r toglogchiig 0, 2-r toglogchiig 1 gej temdegley
-var activePlayer = 1;
+var activePlayer = 0;
 
 
 // Toglogchiin tsugluulssan onoog hadgalah huvisagch
@@ -22,12 +22,45 @@ document.getElementById('current-0').textContent = "0";
 document.getElementById('current-1').textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+// shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
+    //1-6 dotorh sanamsargui 1 too buulgah
     var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+//shoonii zurgiig web deer gargaj irne
     diceDom.style.display = "block";
-    diceDom.src = "dice-" + diceNumber + ".png" ;
+    //buusan shoonii toond hargalzan shoonii zurgiig web deer gargaj irne
+    diceDom.src = "dice-" + diceNumber + ".png";
+    //Buusan too ni 1ees yalgaatai bol idvehtei toglogchiin eeljiin onoog nemegduulne
+    if (diceNumber !== 1) {
+        // 1ees yalgaatai too buulaa. Buusan toog toglogchid nemj ogno.
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+
+
+    } else {
+        // 1 buusan tul toglogchiin eeljiig ene hesegt solij ogno.
+         //Ene toglogchiin eeljindee tsugluulsan onoog 0 bolgono
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        // toglogchiin eeljiig nogoo talruu shiljuulne
+        //Herev idevhtei toglogch ni 0 baival idevhtei toglogchiig 1 bolgo.
+        //Ugui bol idevhtei toglogchiig 0 bolgo.
+     
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+        //Ulaan tsegiig shiljuuleh
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //shoog tur alga bolgoh
+        diceDom.style.display = "none";
+        // if (activePlayer === 0) {
+        //     activePlayer = 1;
+
+        // } else {
+        //     activePlayer = 0;
+        // }
+    }
 });
 
 
